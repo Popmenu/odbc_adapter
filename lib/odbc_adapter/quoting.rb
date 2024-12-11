b11e2e5b2a564f8df4ea6a257b5aaa04b64e7069
Justis Blasco <justis.blasco@gmail.com>
2024-12-11 01:10:10 -0500

    Hardcode quoting config to avoid DB connection

--- lib/odbc_adapter/quoting.rb
+++ lib/odbc_adapter/quoting.rb
@@ -11,7 +11,7 @@ module ODBCAdapter
       # Returns a quoted form of the column name.
       def quote_column_name(name)
         name = name.to_s
-        quote_char = database_metadata.identifier_quote_char.to_s.strip
+        quote_char = "\""
 
         return name if quote_char.length.zero?
         quote_char = quote_char[0]
@@ -19,10 +19,8 @@ module ODBCAdapter
         # Avoid quoting any already quoted name
         return name if name[0] == quote_char && name[-1] == quote_char
 
-        # If upcase identifiers, only quote mixed case names.
-        if database_metadata.upcase_identifiers?
-          return name unless name =~ /([A-Z]+[a-z])|([a-z]+[A-Z])/
-        end
+        # Upcase identifiers only quote mixed case names
+        return name unless name =~ /([A-Z]+[a-z])|([a-z]+[A-Z])/
 
         "#{quote_char.chr}#{name}#{quote_char.chr}"
       end
